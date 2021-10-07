653f4f48acd72c014e84812c666fbeeba2d158a7
AntonMakarkin <amakarkin27@gmail.com>
2021-10-07 22:02:19 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config()
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('App is running')
+})
+
+const CONNECTION_URL = process.env.MONGODB_URL
+const PORT = process.env.PORT || 5001;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`))
+
+mongoose.set('useFindAndModify', false)
