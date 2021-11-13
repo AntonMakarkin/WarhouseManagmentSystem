f027a2b8f57e0aa08b808e288b33c0dcf11d1674
AntonMakarkin <amakarkin27@gmail.com>
2021-11-13 21:57:26 +0300

    Finished route for brands. Need to add types in category model

--- server/models/catalog/brands.js
+++ server/models/catalog/brands.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const brandSchema = new Schema({
-    name: { type: String, unique: true , required: true }
+    name: { type: String, unique: true , required: true, lowercase: true }
 });
 
 const Brand = model('Brand', brandSchema);
--- server/routes/brands.js
+++ server/routes/brands.js
@@ -4,117 +4,26 @@ const Admin = require('../models/users/admin');
 const Manager = require('../models/users/manager');
 const StoreKeeper = require('../models/users/storeKeeper');
 const auth = require('../middleware/auth');
+const catalogControllers = require('../controllers/catalog');
+const addInCatalog = catalogControllers.addInCatalog;
+const getFromCatalog = catalogControllers.getFromCatalog;
+const deleteAllFromCatalog = catalogControllers.deleteAllFromCatalog;
+const searchItemsFromCatalog = catalogControllers.searchItemsFromCatalog;
+const searchItemById = catalogControllers.searchItemById;
+const updateItemById = catalogControllers.updateItemFromCatalogById;
+const deleteItemById = catalogControllers.deleteItemById;
 
 const router = express.Router();
 
 //routes
-router.post('/brands', auth([Admin, Manager, StoreKeeper]), async (req, res) => {
-    const { name } = req.body;
+router.post('/brands', auth([Admin, Manager, StoreKeeper]), addInCatalog(Brand));
+router.get('/brands', getFromCatalog(Brand, 'Бренды'));
+router.delete('/brands', auth([Admin]), deleteAllFromCatalog(Brand, 'Бренды'));
 
-    const item = new Brand({ name });
+router.get('/brands/search', searchItemsFromCatalog(Brand));
 
-    try {
-        await item.save();
-        res.status(201).json({ item });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-});
-
-router.get('/brands', async (req, res) => {
-    try {
-        const items = await Brand.find({});
-
-        if (!items) {
-            throw new Error('Информация о брендах отсутcтвует!');
-        }
-
-        res.json({ items });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
-});
-
-router.delete('/brands', auth([Admin]), async (req, res) => {
-    try {
-        await Brand.deleteMany({});
-        res.json({ message: 'Список брендов успешно очищен' });  
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
-
-router.get('/brands/search', async (req, res) => {
-    const { searchQuery } = req.query;
-
-    try {
-        const title = new RegExp(searchQuery, 'i');
-
-        const items = await Brand.find({ name: title });
-
-        res.json({ items });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
-});
-
-router.get('/brands/:id', async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const item = await Brand.findById(id);
-
-        if (!item) {
-            throw new Error('Бренд с данным id не найден!');
-        }
-
-        res.json({ item });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
-});
-
-router.patch('/brands/:id', auth([Admin, Manager, StoreKeeper]), async (req, res) => {
-    const { id } = req.params;
-
-    const item = await Brand.findById(id);
-
-    if (!item) {
-        return res.status(400).send({ error: 'Бренд с данным id не найден!' });
-    }
-
-    const updates = Object.keys(req.body); //return array of properties
-    const allowedUpdates = ['name'];
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Невозможно обновить данные параметры записи!' });
-    }
-
-    try {
-        updates.forEach((update) => item[update] = req.body[update]); //updating the user
-        await item.save();
-        res.json({ item });
-    } catch (err) {
-        res.status(404).send({ error: err.message });
-    }
-});
-
-router.delete('/brands/:id', auth([Admin, Manager, StoreKeeper]), async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const item = await Brand.findById(id);
-
-        if (!item) {
-            throw new Error('Бренд с данным id не найден!');
-        }
-
-        await Brand.deleteOne(item);
-        res.status(200).send({ message: 'Бренд успешно удален' });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
-});
+router.get('/brands/:id', searchItemById(Brand, 'Бренд'));
+router.patch('/brands/:id', auth([Admin, Manager, StoreKeeper]), updateItemById(Brand, 'Бренд'));
+router.delete('/brands/:id', auth([Admin, Manager, StoreKeeper]), deleteItemById(Brand, 'Бренд'));
 
 module.exports = router;
